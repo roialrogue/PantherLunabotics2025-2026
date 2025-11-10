440a33a755b847e6f351847737e73766f42bd9d2
george-nehma <gnehma2020@my.fit.edu>
2025-11-10 17:59:16 -0500

    working on server client communication

--- onboard_software/robot.py
+++ onboard_software/robot.py
@@ -15,10 +15,10 @@ class Robot:
         self.server.start()
 
         data = self.server.cmd_input_queue.get()
-        if data == "TELEOP START":
+        if data[0] == "TELEOP START":
             self.teleop_ctrl.start()
             self.mode = "TELEOP"
-        elif data == "AUTONOMOUS START":
+        elif data[0] == "AUTONOMOUS START":
             self.auto_ctrl.start()
             self.mode = "AUTO"
 
@@ -36,18 +36,22 @@ class Robot:
             self.mode = "AUTO"
 
     def run(self):
-        
+        id = self.mode # startup mode initialisation
         try:
             while True:
                 try:
                     data = self.server.cmd_input_queue.get_nowait()
+                    id = data[0]
+                    print(id)
+                    if id.startswith("SWITCH"):
+                         self.toggle_mode(id)
+
+      
                 except queue.Empty:
                     data = None
 
-                self.toggle_mode(data)
                 cmd = data
-                id = cmd[0]
-
+                print(self.mode)
                 if self.mode == "AUTO" and id == "AUTO":
                     telem = self.auto_ctrl.run_step(cmd)
                     self.server.telem_output_queue.put(telem)
