dcb6afc78f8135eb831dfc29ead7d568353375ac
Asher Plihal <aplihal2025@my.fit.edu>
2026-02-27 20:09:34 -0500

    up power

--- onboard_software/subsystems/auger.py
+++ onboard_software/subsystems/auger.py
@@ -29,10 +29,10 @@ class Auger:
         self.mc.set_motor_duty_cycle(self.motor_id, power)
 
     def intake(self):
-        self.set_power(0.05)
+        self.set_power(0.2)
 
     def outtake(self):
-        self.set_power(-0.05)
+        self.set_power(-0.2)
 
     def stop(self):
         self.set_power(0.0)
