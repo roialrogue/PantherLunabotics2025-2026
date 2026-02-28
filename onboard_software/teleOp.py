e6e80228e6c13a1323b86d228e40ede1db33edef
Asher Plihal <aplihal2025@my.fit.edu>
2026-02-27 20:02:13 -0500

    commented out driving code

--- onboard_software/teleOp.py
+++ onboard_software/teleOp.py
@@ -17,42 +17,42 @@ class TeleOp:
         if button == 'A':
             if is_pressed:
                 #Forward
-                self.robot.drivetrain.set_power(-0.5,0.5,-0.5,0.5)
+                #self.robot.drivetrain.set_power(-0.5,0.5,-0.5,0.5)
                 print("[TELEOP] A button pressed")
             else:
                 print("[TELEOP] A button released")
         elif button == 'B':
             if is_pressed:
                 #strafe right
-                self.robot.drivetrain.set_power(-0.5,0.5,0.5,-0.5)
+                #self.robot.drivetrain.set_power(-0.5,0.5,0.5,-0.5)
                 print("[TELEOP] B button pressed")
             else:
                 print("[TELEOP] B button released")
         elif button == 'X':
             if is_pressed:
                 #strafe left
-                self.robot.drivetrain.set_power(0.5,-0.5,-0.5,0.5)
+                #self.robot.drivetrain.set_power(0.5,-0.5,-0.5,0.5)
                 print("[TELEOP] X button pressed")
             else:
                 print("[TELEOP] X button released")
         elif button == 'Y':
             if is_pressed:
                 #backward
-                self.robot.drivetrain.set_power(0.5,-0.5,0.5,-0.5)
+                #self.robot.drivetrain.set_power(0.5,-0.5,0.5,-0.5)
                 print("[TELEOP] Y button pressed")
             else:
                 print("[TELEOP] Y button released")
         elif button == 'LB':
             if is_pressed:
                 #Turn left
-                self.robot.drivetrain.set_power(-0.5,-0.5,-0.5,-0.5)
+                #self.robot.drivetrain.set_power(-0.5,-0.5,-0.5,-0.5)
                 print("[TELEOP] LB button pressed")
             else:
                 print("[TELEOP] LB button released")
         elif button == 'RB':
             if is_pressed:
                 #Turn right
-                self.robot.drivetrain.set_power(0.5,0.5,0.5,0.5)
+                #self.robot.drivetrain.set_power(0.5,0.5,0.5,0.5)
                 print("[TELEOP] RB button pressed")
             else:
                 print("[TELEOP] RB button released")
